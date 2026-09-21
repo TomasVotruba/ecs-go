213707df24d030c98cf8f28e2bef5fa256d3fd53
Tomas Votruba <tomas.vot@gmail.com>
2026-09-21 11:54:59 +0200

    gofmt priorities.go

--- ecs-go/internal/fixer/rules/priorities.go
+++ ecs-go/internal/fixer/rules/priorities.go
@@ -45,7 +45,7 @@ var fixerPriority = map[string]int{
 	"ExplicitIndirectVariableFixer":                   0,
 	"ExplicitStringVariableFixer":                     6,
 	"FullOpeningTagFixer":                             98,
-	"FullyQualifiedStrictTypesFixer":                   7,
+	"FullyQualifiedStrictTypesFixer":                  7,
 	"FunctionDeclarationFixer":                        31,
 	"FunctionToConstantFixer":                         2,
 	"HeredocToNowdocFixer":                            0,
